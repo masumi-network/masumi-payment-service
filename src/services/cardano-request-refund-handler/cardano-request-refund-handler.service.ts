ea06d888fb62f3c019d732a63b52ae9751554553
Isaac <bytegenlabs@gmail.com>
2025-07-24 15:30:28 +0100

    revert: remove all backend changes to maintain frontend-only feature

--- src/services/cardano-request-refund-handler/cardano-request-refund-handler.service.ts
+++ src/services/cardano-request-refund-handler/cardano-request-refund-handler.service.ts
@@ -147,8 +147,8 @@ export async function requestRefundsV1() {
                 BigInt(decodedContract.externalDisputeUnlockTime) ==
                   BigInt(request.externalDisputeUnlockTime) &&
                 BigInt(decodedContract.collateralReturnLovelace) ==
-                  BigInt(request.collateralReturnLovelace) &&
-                BigInt(decodedContract.payByTime) == BigInt(request.payByTime)
+                  BigInt(request.collateralReturnLovelace!) &&
+                BigInt(decodedContract.payByTime) == BigInt(request.payByTime!)
               );
             });
 
@@ -255,7 +255,7 @@ export async function requestRefundsV1() {
                 address,
                 utxo,
                 sortedUtxosByLovelaceDesc[0],
-                sortedUtxosByLovelaceDesc,
+                limitedUtxos,
                 datum.value,
                 invalidBefore,
                 invalidAfter,
--- src/services/cardano-submit-result-handler/cardano-submit-result-handler.service.ts
+++ src/services/cardano-submit-result-handler/cardano-submit-result-handler.service.ts
@@ -150,8 +150,8 @@ export async function submitResultV1() {
                 BigInt(decodedContract.externalDisputeUnlockTime) ==
                   BigInt(request.externalDisputeUnlockTime) &&
                 BigInt(decodedContract.collateralReturnLovelace) ==
-                  BigInt(request.collateralReturnLovelace) &&
-                BigInt(decodedContract.payByTime) == BigInt(request.payByTime)
+                  BigInt(request.collateralReturnLovelace!) &&
+                BigInt(decodedContract.payByTime) == BigInt(request.payByTime!)
               );
             });
 
@@ -261,7 +261,7 @@ export async function submitResultV1() {
                 address,
                 utxo,
                 sortedUtxosByLovelaceDesc[0],
-                sortedUtxosByLovelaceDesc,
+                limitedUtxos,
                 datum.value,
                 invalidBefore,
                 invalidAfter,
