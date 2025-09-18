50bebfabd42df0db9862d5350b48f9d508715aa8
Cryptonean <faizanvit8@gmail.com>
2025-09-18 18:26:47 +0530

    feat(earning): database file

---
+++ prisma/migrations/20250917100830_add_payment_type_none/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX "RegistryRequest_agentIdentifier_idx";
+
+-- DropIndex
+DROP INDEX "RegistryRequest_paymentSourceId_idx";
+
+-- DropIndex
+DROP INDEX "RegistryRequest_state_idx";
