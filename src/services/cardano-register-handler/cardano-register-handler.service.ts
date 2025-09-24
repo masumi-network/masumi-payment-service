087159b8ea04f1c84035ae630b44168f6af1190a
Cryptonean <faizanvit8@gmail.com>
2025-09-24 13:05:47 +0530

    refactor(register): refactor the register service

--- src/services/cardano-register-handler/cardano-register-handler.service.ts
+++ src/services/cardano-register-handler/cardano-register-handler.service.ts
@@ -16,13 +16,14 @@ import { logger } from '@/utils/logger';
 import { convertNetwork } from '@/utils/converter/network-convert';
 import { generateWalletExtended } from '@/utils/generator/wallet-generator';
 import { lockAndQueryRegistryRequests } from '@/utils/db/lock-and-query-registry-request';
-import { DEFAULTS } from '@/utils/config';
+import { DEFAULTS, SERVICE_CONSTANTS } from '@/utils/config';
 import { getRegistryScriptFromNetworkHandlerV1 } from '@/utils/generator/contract-generator';
 import { blake2b } from 'ethereum-cryptography/blake2b';
 import { stringToMetadata } from '@/utils/converter/metadata-string-convert';
 import { advancedRetryAll, delayErrorResolver } from 'advanced-retry';
 import { Mutex, MutexInterface, tryAcquire } from 'async-mutex';
 import { convertErrorString } from '@/utils/converter/error-string-convert';
+import { sortAndLimitUtxos } from '@/utils/utxo';
 
 const mutex = new Mutex();
 
@@ -43,7 +44,7 @@ export async function registerAgentV1() {
 
     await Promise.allSettled(
       paymentSourcesWithWalletLocked.map(async (paymentSource) => {
-        if (paymentSource.RegistryRequest.length == 0) return;
+        if (paymentSource.RegistryRequest.length === 0) return;
 
         logger.info(
           `Registering ${paymentSource.RegistryRequest.length} agents for payment source ${paymentSource.id}`,
@@ -53,7 +54,7 @@ export async function registerAgentV1() {
 
         const registryRequests = paymentSource.RegistryRequest;
 
-        if (registryRequests.length == 0) return;
+        if (registryRequests.length === 0) return;
 
         const blockchainProvider = new BlockfrostProvider(
           paymentSource.PaymentSourceConfig.rpcProviderApiKey,
@@ -62,37 +63,18 @@ export async function registerAgentV1() {
         const results = await advancedRetryAll({
           errorResolvers: [
             delayErrorResolver({
-              configuration: {
-                maxRetries: 5,
-                backoffMultiplier: 5,
-                initialDelayMs: 500,
-                maxDelayMs: 7500,
-              },
+              configuration: SERVICE_CONSTANTS.RETRY,
             }),
           ],
           operations: registryRequests.map((request) => async () => {
-            if (
-              request.Pricing.pricingType != PricingType.Fixed &&
-              request.Pricing.pricingType != PricingType.Free
-            ) {
-              throw new Error(
-                'Other than fixed and free pricing is not supported yet',
-              );
+            if (request.Pricing.pricingType !== PricingType.Fixed) {
+              throw new Error('Other than fixed pricing is not supported yet');
             }
             if (
-              request.Pricing.pricingType == PricingType.Fixed &&
-              (request.Pricing.FixedPricing == null ||
-                request.Pricing.FixedPricing.Amounts.length == 0)
+              request.Pricing.FixedPricing === null ||
+              request.Pricing.FixedPricing.Amounts.length === 0
             ) {
               throw new Error('No fixed pricing found, this is likely a bug');
-            }
-            if (
-              request.Pricing.pricingType == PricingType.Free &&
-              request.Pricing.FixedPricing != null
-            ) {
-              throw new Error(
-                'Free pricing requires no fixed pricing to be set',
-              );
             }
             const { wallet, utxos, address } = await generateWalletExtended(
               paymentSource.network,
@@ -106,25 +88,7 @@ export async function registerAgentV1() {
             const { script, policyId } =
               await getRegistryScriptFromNetworkHandlerV1(paymentSource);
 
-            const utxosSortedByLovelaceDesc = utxos.sort((a, b) => {
-              const aLovelace = parseInt(
-                a.output.amount.find(
-                  (asset) => asset.unit == 'lovelace' || asset.unit == '',
-                )?.quantity ?? '0',
-              );
-              const bLovelace = parseInt(
-                b.output.amount.find(
-                  (asset) => asset.unit == 'lovelace' || asset.unit == '',
-                )?.quantity ?? '0',
-              );
-              //sort by biggest lovelace
-              return bLovelace - aLovelace;
-            });
-
-            const limitedFilteredUtxos = utxosSortedByLovelaceDesc.slice(
-              0,
-              Math.min(4, utxosSortedByLovelaceDesc.length),
-            );
+            const limitedFilteredUtxos = sortAndLimitUtxos(utxos);
 
             const firstUtxo = limitedFilteredUtxos[0];
             const collateralUtxo = limitedFilteredUtxos[0];
@@ -168,21 +132,14 @@ export async function registerAgentV1() {
                 other: stringToMetadata(request.other),
               },
               tags: request.tags,
-              agentPricing:
-                request.Pricing.pricingType == PricingType.Fixed
-                  ? {
-                      pricingType: PricingType.Fixed,
-                      fixedPricing:
-                        request.Pricing.FixedPricing?.Amounts.map(
-                          (pricing) => ({
-                            unit: stringToMetadata(pricing.unit),
-                            amount: pricing.amount.toString(),
-                          }),
-                        ) ?? [],
-                    }
-                  : {
-                      pricingType: PricingType.Free,
-                    },
+              agentPricing: {
+                pricingType: request.Pricing.pricingType,
+                fixedPricing:
+                  request.Pricing.FixedPricing?.Amounts.map((pricing) => ({
+                    unit: stringToMetadata(pricing.unit),
+                    amount: pricing.amount.toString(),
+                  })) ?? [],
+              },
               image: stringToMetadata(DEFAULTS.DEFAULT_IMAGE),
               metadata_version: request.metadataVersion.toString(),
             };
@@ -261,7 +218,7 @@ export async function registerAgentV1() {
         let index = 0;
         for (const result of results) {
           const request = registryRequests[index];
-          if (result.success == false || result.result != true) {
+          if (result.success === false || result.result !== true) {
             const error = result.error;
             logger.error(`Error registering agent ${request.id}`, {
               error: error,
@@ -316,10 +273,7 @@ async function generateRegisterAgentTransaction(
   exUnits: {
     mem: number;
     steps: number;
-  } = {
-    mem: 7e6,
-    steps: 3e9,
-  },
+  } = SERVICE_CONSTANTS.SMART_CONTRACT.defaultExUnits,
 ) {
   const txBuilder = new MeshTxBuilder({
     fetcher: blockchainProvider,
@@ -333,7 +287,7 @@ async function generateRegisterAgentTransaction(
     .mint('1', policyId, assetName)
     .mintingScript(script.code)
     .mintRedeemerValue({ alternative: 0, fields: [] }, 'Mesh', exUnits)
-    .metadataValue(721, {
+    .metadataValue(SERVICE_CONSTANTS.METADATA.nftLabel, {
       [policyId]: {
         [assetName]: metadata,
       },
@@ -344,10 +298,16 @@ async function generateRegisterAgentTransaction(
       collateralUtxo.input.txHash,
       collateralUtxo.input.outputIndex,
     )
-    .setTotalCollateral('5000000')
+    .setTotalCollateral(SERVICE_CONSTANTS.SMART_CONTRACT.collateralAmount)
     .txOut(walletAddress, [
-      { unit: policyId + assetName, quantity: '1' },
-      { unit: 'lovelace', quantity: '5000000' },
+      {
+        unit: policyId + assetName,
+        quantity: SERVICE_CONSTANTS.SMART_CONTRACT.mintQuantity,
+      },
+      {
+        unit: 'lovelace',
+        quantity: SERVICE_CONSTANTS.SMART_CONTRACT.collateralAmount,
+      },
     ]);
   for (const utxo of utxos) {
     txBuilder.txIn(utxo.input.txHash, utxo.input.outputIndex);
@@ -355,7 +315,9 @@ async function generateRegisterAgentTransaction(
   return await txBuilder
     .requiredSignerHash(deserializedAddress.pubKeyHash)
     .setNetwork(network)
-    .metadataValue(674, { msg: ['Masumi', 'RegisterAgent'] })
+    .metadataValue(SERVICE_CONSTANTS.METADATA.masumiLabel, {
+      msg: ['Masumi', 'RegisterAgent'],
+    })
     .changeAddress(walletAddress)
     .complete();
 }
