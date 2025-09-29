d4cc8c687e0559155c6d82e1de901b236973cb6f
Cryptonean <faizanvit8@gmail.com>
2025-09-29 19:12:54 +0530

    feat(earning):  remove unwanted sql file

--- prisma/migrations/20250916095907_add_registry_performance_indexes/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX IF NOT EXISTS "RegistryRequest_agentIdentifier_idx" ON "RegistryRequest"("agentIdentifier");
-
--- CreateIndex  
-CREATE INDEX IF NOT EXISTS "RegistryRequest_paymentSourceId_idx" ON "RegistryRequest"("paymentSourceId");
-
--- CreateIndex
-CREATE INDEX IF NOT EXISTS "RegistryRequest_state_idx" ON "RegistryRequest"("state");
--- prisma/migrations/20250917100830_add_payment_type_none/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "RegistryRequest_agentIdentifier_idx";
-
--- DropIndex
-DROP INDEX "RegistryRequest_paymentSourceId_idx";
-
--- DropIndex
-DROP INDEX "RegistryRequest_state_idx";
