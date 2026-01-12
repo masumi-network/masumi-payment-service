aff3ae503f364ea0766c168fcff90bce9c6deabd
Sandro Schaier <sandro@schaier.io>
2026-01-12 13:52:18 +0100

    fix: changes to cbor decoding

--- src/utils/converter/public-key-convert/index.ts
+++ src/utils/converter/public-key-convert/index.ts
@@ -1,14 +1,14 @@
 import { Ed25519PublicKey } from '@meshsdk/core-cst';
 import { Cbor, CborNegInt, CborMap, CborBytes } from '@harmoniclabs/cbor';
-import JSONBig from 'json-bigint';
 
 export function getPublicKeyFromCoseKey(cbor: string): Ed25519PublicKey | null {
   const decodedCoseKey = Cbor.parse(cbor) as CborMap;
   const publicKeyEntry = decodedCoseKey.map.find((value) => {
-    return (
-      JSONBig.stringify(value.k) ===
-      JSONBig.stringify(new CborNegInt(BigInt(-2)))
-    );
+    const k = value.k;
+    if (k instanceof CborNegInt) {
+      return k.num === BigInt(-2);
+    }
+    return false;
   });
 
   if (publicKeyEntry) {
