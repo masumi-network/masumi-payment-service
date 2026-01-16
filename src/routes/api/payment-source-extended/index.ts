5b52ec64de10597eb7e1d932d9391283461d0a39
Sandro Schaier <sandro@schaier.io>
2026-01-16 21:32:12 +0100

    fix: eslint error

--- src/routes/api/payment-source-extended/index.ts
+++ src/routes/api/payment-source-extended/index.ts
@@ -6,7 +6,7 @@ import { prisma } from '@/utils/db';
 import { encrypt } from '@/utils/security/encryption';
 import { adminAuthenticatedEndpointFactory } from '@/utils/security/auth/admin-authenticated';
 import { resolvePaymentKeyHash } from '@meshsdk/core-cst';
-import { HotWalletType, RPCProvider, Network, $Enums } from '@prisma/client';
+import { HotWalletType, RPCProvider, Network } from '@prisma/client';
 import createHttpError from 'http-errors';
 import { z } from '@/utils/zod-openapi';
 import { generateOfflineWallet } from '@/utils/generator/wallet-generator';
