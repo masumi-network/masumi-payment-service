10a8fb1a1ed4637ce4c4a38ca7806d7da491f1ec
Cryptonean <faizanvit8@gmail.com>
2026-03-18 10:02:41 +0530

    fix(crypto): split api-key hash into separate module

--- src/utils/crypto/index.ts
+++ src/utils/crypto/index.ts
@@ -1,5 +1,4 @@
 import crypto from 'crypto';
-import { CONFIG } from '@/utils/config';
 
 const generateSHA256Hash = (data: string) => {
 	return crypto.createHash('sha256').update(data).digest('hex');
@@ -7,22 +6,5 @@ const generateSHA256Hash = (data: string) => {
 
 const generateHash = generateSHA256Hash;
 
-let _apiKeySalt: Buffer | undefined;
-const _getApiKeySalt = (): Buffer => {
-	if (!_apiKeySalt) {
-		_apiKeySalt = crypto.scryptSync(CONFIG.ENCRYPTION_KEY, 'masumi-apikey-pbkdf2-salt-v1', 32);
-	}
-	return _apiKeySalt;
-};
-
-const generateApiKeySecureHash = (token: string): Promise<string> => {
-	const salt = _getApiKeySalt();
-	return new Promise((resolve, reject) => {
-		crypto.pbkdf2(token, salt, 100_000, 64, 'sha512', (err, derivedKey) => {
-			if (err) reject(err);
-			else resolve(derivedKey.toString('hex'));
-		});
-	});
-};
-
-export { generateHash, generateSHA256Hash, generateApiKeySecureHash };
+export { generateHash, generateSHA256Hash };
+export { generateApiKeySecureHash } from './api-key-hash';
