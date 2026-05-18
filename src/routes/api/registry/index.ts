f1c1ae135460b6afc3d73b74e5de8b077fea0771
Cryptonean <faizanvit8@gmail.com>
2026-05-18 08:21:00 +0530

    chore: conflicts solved and dev merge

--- src/routes/api/registry/index.ts
+++ src/routes/api/registry/index.ts
@@ -143,7 +143,7 @@ export const registerAgentPost = payAuthenticatedEndpointFactory.build({
 					privacyPolicy: input.Legal?.privacyPolicy,
 					authorName: input.Author.name,
 					paymentType:
-						input.AgentPricing.pricingType == PricingType.Fixed ? PaymentType.None : PaymentType.Web3CardanoV1,
+						input.AgentPricing.pricingType == PricingType.Free ? PaymentType.Web3CardanoV1 : PaymentType.None,
 					authorContactEmail: input.Author.contactEmail,
 					authorContactOther: input.Author.contactOther,
 					authorOrganization: input.Author.organization,
