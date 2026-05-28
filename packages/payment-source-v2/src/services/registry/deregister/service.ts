88c9dfc0bcc3ed329020e266605394e9d36529cf
Sandro Schaier <80533393+schaier-io@users.noreply.github.com>
2026-05-28 22:45:25 +0700

    fix: linting issue

--- packages/payment-source-v2/src/services/registry/deregister/service.ts
+++ packages/payment-source-v2/src/services/registry/deregister/service.ts
@@ -19,7 +19,6 @@ import {
 	createPendingTransaction,
 	disconnectTransactionWallet,
 	loadHotWalletSession,
-	updateCurrentTransactionHash,
 } from '@/services/shared';
 import {
 	findRegistryTokenUtxo,
